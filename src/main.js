79701b1549b28a3284365beb92d61393a659e7a3
AlexanderPri <165932672+AlexanderPri@users.noreply.github.com>
2024-04-28 17:45:26 +0000

    Cambios

--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
 import Carousel from 'react-bootstrap/Carousel';
 const Main = () => {
     return <main>
+      <br></br>
     <Carousel>
       <Carousel.Item>
-      <img src="https://elcomercio.pe/resizer/MDKz8INVeIBwI_jkSE2EYsChg-Q=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/NTKU3EROOZDVPCWTEQRRWFDV54.jpg" className="d-block w-100" alt="..."></img>
+      <img src="https://elcomercio.pe/resizer/MDKz8INVeIBwI_jkSE2EYsChg-Q=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/NTKU3EROOZDVPCWTEQRRWFDV54.jpg" className="d-block carousel-image" alt="..."></img>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="https://www.checkealo.pe/files/productos/6134/e08d57bdd004d0ac20406efd2e9ae02b.jpg" className="d-block w-100" alt="..."></img>
+      <img src="https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg" className="d-block carousel-image" alt="..."></img>
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="https://entrenosotros.consum.es/public/Image/2022/5/makis.jpg" className="d-block w-100" alt="..."></img> 
+      <img src="https://entrenosotros.consum.es/public/Image/2022/5/makis.jpg" className="d-block carousel-image" alt="..."></img> 
         <Carousel.Caption>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
 
  <h1>CONOCE SOBRE NOSOTROS</h1>
- <p> Ven a nuestra tienda especializada en la prepación de
-comida japonesa. Ordena todos y cada uno de nuestros platos a un precio
-razonable y comparte el sabor de la cultura asiática.</p>
+ <p>¡Bienvenidos al paraíso del sushi! En nuestra tienda, cada bocado es una obra maestra de frescura y sabor. Sumérgete en la exquisita tradición culinaria japonesa mientras te deleitas con nuestra amplia variedad de rollos de sushi, nigiri y sashimi, preparados con los ingredientes más selectos y cuidadosamente elaborados por nuestros expertos chefs.</p>
 
     </main>;
 }
