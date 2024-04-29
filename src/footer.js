522f878d41dca5720ad006ec3d7e32b50ed44bda
AlexanderPri <165932672+AlexanderPri@users.noreply.github.com>
2024-04-29 00:43:17 +0000

    Actualizado2

--- src/footer.js
+++ src/footer.js
@@ -4,26 +4,37 @@ const Footer = () => {
             <footer className='text-white py-4 bg-dark'>
                 <div className='container'>
                     <nav className='row'>
-                        <ul className='col-12 col-md-3 list-unstyled'>
-                            <li className='font-weight-bold mb-2'>SushiBar</li>
-                            <h7 className='text-center'><em>"SushiBar es más que un restaurante, es un hogar y lo demostramos en la calidez de nuestra atención en cada visita a esta casa."</em></h7>
+                        <ul className='col-12 col-md-3 list-unstyled with-divider'>
+                            <li className='font-weight-bold mb-2'><b>SushiBar</b></li>
+                            <h7 className='text-center Mod2'><em>"SushiBar es más que un restaurante, es un hogar y lo demostramos en la calidez de nuestra atención en cada visita a esta casa."</em></h7>
                         </ul>
-                        <ul className='col-12 col-md-3 list-unstyled'>
-                            <li className='font-weight-bold mb-2'>Mapa del sitio</li>
-                            
+                        <ul className='col-12 col-md-3 list-unstyled with-divider'>
+                            <li className='font-weight-bold mb-2'><b>Mapa del sitio</b></li>
+                            <li className='font-weight-bold mb-2'><a className='Saavedra' href="#">Inicio</a></li>
+                            <li className='font-weight-bold mb-2'><a className='Saavedra' href="#">Nuestra historia</a></li>
+                            <li className='font-weight-bold mb-2'><a className='Saavedra' href="#">Nuestra carta</a></li>
+                            <li className='font-weight-bold mb-2'><a className='Saavedra' href="#">Libro de reclamaciones</a></li>           
                         </ul>
-                        <ul className='col-12 col-md-3 list-unstyled'>
-                            <li className='font-weight-bold mb-2'>SushiBar</li>
+                        <ul className='col-12 col-md-3 list-unstyled with-divider'>
+                            <li className='font-weight-bold mb-2'><b>Horarios de atención</b></li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="calendar"></i> Lunes a viernes 1 p.m. - 11:59 p.m.</li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="calendar"></i> Sabados 4 p.m. - 10:00 p.m.</li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="calendar"></i> Domingos 4 p.m. - 10:00 p.m. (solo delivery) </li>
                         </ul>
-                        <ul className='col-12 col-md-3 list-unstyled'>
-                            <li className='font-weight-bold mb-2'>Encuentranos en</li>
-                            <li className='font-weight-bold mb-2'><i data-feather="map-pin"></i> Jr. Bolívar 957, Trujillo, Perú</li>
-                            <li className='font-weight-bold mb-2'><i data-feather="phone"></i>+51 955 109 474</li>
-                            <li className='font-weight-bold mb-2'><i data-feather="instagram"></i> https://www.instagram.com/SushiBar/</li>
-                            <li className='font-weight-bold mb-2'><i data-feather="mail"></i> chunchunmaru1478@gmail.com</li>
+                        <ul className='col-12 col-md-3 list-unstyled with-divider'>
+                            <li className='font-weight-bold mb-2'><b>Encuentranos en</b></li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="map-pin"></i> Jr. Bolívar 957, Trujillo, Perú</li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="phone"></i> +51 955 109 474</li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="instagram"></i> <a className="Mod" href="#">https://www.instagram.com/SushiBar/</a></li>
+                            <li className='font-weight-bold mb-2 Mod2'><i data-feather="mail"></i> <a className="Mod" href="#">chunchunmaru1478@gmail.com</a></li>
                         </ul>
                     </nav>
                 </div>
+                <section className='footer-bottom'>
+                    <div className='container'>
+                        <p className='abajocreo Mod2'>Copyright © 2019 - 2024 Sushi.com | Todos los derechos reservados ha Alexander Prieto</p>
+                    </div>
+                </section>
             </footer>
         </div>
     </footer>;
--- src/header.js
+++ src/header.js
@@ -19,12 +19,6 @@ const Header = () => {
         <a className="nav-link" href="#"><i data-feather="help-circle"></i>Ayuda</a>
       </div>
     </div>
-    <form className="d-flex" role="search">
- <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-    <button className="btn btn-outline-success" type="submit">
-        <i data-feather="search"></i>
-    </button>
- </form>
  </div>
 </nav>
         </header>;
